4069af93d92133a0bda114d8d6361831301bb496
sebastian <manijustworkhere@gmail.com>
2025-08-19 19:39:11 -0400

    man im kinda following along but there is so much the video is doing that is so similar theres book, books, books.html, getBooks, books.map, book.url.   man im trying idk if its bc all the caffene or, the partial dyslexia, or just adhd rooted short term memory loss but im pretty dam confuzzled

--- books.js
+++ books.js
@@ -1,11 +1,52 @@
+function renderBooks() {
+const booksWrapper = document.querySelector('.books');
 
+
+const books = getBooks();
+
+const booksHTML = books.map((book) => {
+  return `<div class="book">
+        <figure class="book__img--wrapper">
+            <img class="book__img" src='${book.url}' alt="">
+        </figure>
+        <div class="book__title">
+           ${book.url}
+        </div>
+        <div class="book__ratings">
+            <i class="fas fa-star"></i>
+            <i class="fas fa-star"></i>
+            <i class="fas fa-star"></i>
+            <i class="fas fa-star"></i>
+            <i class="fas fa-star-half-alt"></i>
+        </div>
+        <div class="book__price">
+            <span class="book__price--normal">$${book.originalPrice}</span> <span class="purple">$${book.salePrice}
+            </span>
+        </div>
+    </div>`
+}).join("")
+
+console.log(booksHTML)
+
+booksWrapper.innerHTML = booksHTML;
+//booksWrapper.innerHTML =
+}
+
+
+
+
+
+
+setTimeout(() => {
+  renderBooks();
+});
 // FAKE DATA
 function getBooks() {
   return [
     {
       id: 1,
       title: "Crack the Coding Interview",
-                url: "assets/crack the coding interview.png",
+      url: "assets/crack the coding interview.png",
       originalPrice: 49.95,
       salePrice: 14.95,
       rating: 4.5,
@@ -14,7 +55,7 @@ function getBooks() {
       id: 2,
       title: "Atomic Habits",
       url: "assets/atomic habits.jpg",
-      originalPrice: 39,
+      originalPrice: 39.95,
       salePrice: null,
       rating: 5,
     },
@@ -22,47 +63,47 @@ function getBooks() {
       id: 3,
       title: "Deep Work",
       url: "assets/deep work.jpeg",
-      originalPrice: 29,
-      salePrice: 12,
+      originalPrice: 29.95,
+      salePrice: 12.95,
       rating: 5,
     },
     {
       id: 4,
       title: "The 10X Rule",
       url: "assets/book-1.jpeg",
-      originalPrice: 44,
-      salePrice: 19,
+      originalPrice: 44.95,
+      salePrice: 19.95,
       rating: 4.5,
     },
     {
       id: 5,
       title: "Be Obsessed Or Be Average",
       url: "assets/book-2.jpeg",
-      originalPrice: 32,
-      salePrice: 17,
+      originalPrice: 32.95,
+      salePrice: 17.95,
       rating: 4,
     },
     {
       id: 6,
       title: "Rich Dad Poor Dad",
       url: "assets/book-3.jpeg",
-      originalPrice: 70,
-      salePrice: 12.5,
+      originalPrice: 70.95,
+      salePrice: 12.50,
       rating: 5,
     },
     {
       id: 7,
       title: "Cashflow Quadrant",
       url: "assets/book-4.jpeg",
-      originalPrice: 11,
-      salePrice: 10,
+      originalPrice: 11.95,
+      salePrice: 10.95,
       rating: 4,
     },
     {
       id: 8,
       title: "48 Laws of Power",
       url: "assets/book-5.jpeg",
-      originalPrice: 38,
+      originalPrice: 38.95,
       salePrice: 17.95,
       rating: 4.5,
     },
@@ -70,7 +111,7 @@ function getBooks() {
       id: 9,
       title: "The 5 Second Rule",
       url: "assets/book-6.jpeg",
-      originalPrice: 35,
+      originalPrice: 35.95,
       salePrice: null,
       rating: 4,
     },
@@ -78,7 +119,7 @@ function getBooks() {
       id: 10,
       title: "Your Next Five Moves",
       url: "assets/book-7.jpg",
-      originalPrice: 40,
+      originalPrice: 40.95,
       salePrice: null,
       rating: 4,
     },
@@ -86,7 +127,7 @@ function getBooks() {
       id: 11,
       title: "Mastery",
       url: "assets/book-8.jpeg",
-      originalPrice: 30,
+      originalPrice: 30.95,
       salePrice: null,
       rating: 4.5,
     },
