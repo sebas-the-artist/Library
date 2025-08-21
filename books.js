cb60ef48ba5c4bd69cf379da0275a4583a8ed0b6
sebastian <manijustworkhere@gmail.com>
2025-08-20 20:59:11 -0400

    i got the sorting function to work but the sale price is giving me shit ill handle it doh

--- books.js
+++ books.js
@@ -1,35 +1,70 @@
-function renderBooks() {
+function renderBooks(filter) {
 const booksWrapper = document.querySelector('.books');
-
 
 const books = getBooks();
 
-const booksHTML = books.map((book) => {
+if (filter === "LOW_TO_HIGH") {
+books.sort((a, b) => a.originalPrice - b.originalPrice);
+}
+else if (filter === "HIGH_TO_LOW") {
+books.sort((a, b) => b.originalPrice - a.originalPrice);
+}
+else if (filter === "RATING") {
+books.sort((a, b) => b.rating - a.rating);
+}
+
+
+
+
+const booksHTML = books
+.map((book) => {
   return `<div class="book">
         <figure class="book__img--wrapper">
             <img class="book__img" src='${book.url}' alt="">
         </figure>
         <div class="book__title">
-           ${book.url}
+           ${book.title}
         </div>
         <div class="book__ratings">
-            <i class="fas fa-star"></i>
-            <i class="fas fa-star"></i>
-            <i class="fas fa-star"></i>
-            <i class="fas fa-star"></i>
-            <i class="fas fa-star-half-alt"></i>
+        ${ratingHTML(book.rating)}
         </div>
         <div class="book__price">
-            <span class="book__price--normal">$${book.originalPrice}</span> <span class="purple">$${book.salePrice}
-            </span>
+           ${priceHTML(book.originalPrice, book.salePrice)}
         </div>
-    </div>`
-}).join("")
-
-console.log(booksHTML)
+  </div>`
+})
+.join("")
 
 booksWrapper.innerHTML = booksHTML;
-//booksWrapper.innerHTML =
+}
+
+
+function priceHTML(originalPrice, salePrice) {
+  if (!salePrice) {
+    return `$${originalPrice.toFixed(2)}`
+  }
+  else {
+    return `<span class="book__price--normal">$${originalPrice}</span> <span class="purple">$${salePrice}</span>`
+  }
+}
+
+
+function ratingHTML(rating) {
+let ratingHTML = "";
+for (let i = 0; i < Math.floor(rating); ++i) {
+  ratingHTML += `<i class="fas fa-star"></i>\n`
+}
+if (!Number.isInteger(rating)) {
+  ratingHTML += `<i class="fas fa-star-half-alt"></i>\n`
+}
+return ratingHTML
+}
+
+
+
+
+function filterBooks(event) {
+  renderBooks(event.target.value);
 }
 
 
@@ -49,7 +84,7 @@ function getBooks() {
       url: "assets/crack the coding interview.png",
       originalPrice: 49.95,
       salePrice: 14.95,
-      rating: 4.5,
+      rating: 4,
     },
     {
       id: 2,
@@ -64,7 +99,7 @@ function getBooks() {
       title: "Deep Work",
       url: "assets/deep work.jpeg",
       originalPrice: 29.95,
-      salePrice: 12.95,
+      salePrice: 14.95,
       rating: 5,
     },
     {
@@ -73,45 +108,45 @@ function getBooks() {
       url: "assets/book-1.jpeg",
       originalPrice: 44.95,
       salePrice: 19.95,
-      rating: 4.5,
+      rating: 4,
     },
     {
       id: 5,
       title: "Be Obsessed Or Be Average",
       url: "assets/book-2.jpeg",
-      originalPrice: 32.95,
-      salePrice: 17.95,
-      rating: 4,
+      originalPrice: 89.95,
+      salePrice: 9.95,
+      rating: 1,
     },
     {
       id: 6,
       title: "Rich Dad Poor Dad",
       url: "assets/book-3.jpeg",
       originalPrice: 70.95,
-      salePrice: 12.50,
-      rating: 5,
+      salePrice: 14.95,
+      rating: 4.5,
     },
     {
       id: 7,
       title: "Cashflow Quadrant",
       url: "assets/book-4.jpeg",
       originalPrice: 11.95,
-      salePrice: 10.95,
+      salePrice: 9.95,
       rating: 4,
     },
     {
       id: 8,
       title: "48 Laws of Power",
       url: "assets/book-5.jpeg",
-      originalPrice: 38.95,
-      salePrice: 17.95,
-      rating: 4.5,
+      originalPrice: 48.48,
+      salePrice: 19.95,
+      rating: 5,
     },
     {
       id: 9,
       title: "The 5 Second Rule",
       url: "assets/book-6.jpeg",
-      originalPrice: 35.95,
+      originalPrice: 34.95,
       salePrice: null,
       rating: 4,
     },
@@ -119,7 +154,7 @@ function getBooks() {
       id: 10,
       title: "Your Next Five Moves",
       url: "assets/book-7.jpg",
-      originalPrice: 40.95,
+      originalPrice: 39.95,
       salePrice: null,
       rating: 4,
     },
@@ -127,10 +162,18 @@ function getBooks() {
       id: 11,
       title: "Mastery",
       url: "assets/book-8.jpeg",
-      originalPrice: 30.95,
+      originalPrice: 29.95,
       salePrice: null,
       rating: 4.5,
     },
+    {
+      id: 12,
+      title: "Can't Hurt Me",
+      url: "assets/david goggins.jpeg",
+      originalPrice: 29.95,
+      salePrice: null,
+      rating: 5,
+    }
   ];
 }
 
