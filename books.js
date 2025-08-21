3aa74cb778d44e6b58c86efaae1dfe2de0a509bf
sebastian <manijustworkhere@gmail.com>
2025-08-21 01:58:46 -0400

    i wouldnt call it done but i guess its about as good as the effort i put into it, ill be back and see what i can do once im further in my learning journey, maybe ill add some react

--- books.js
+++ books.js
@@ -1,13 +1,15 @@
-function renderBooks(filter) {
+async function renderBooks(filter) {
 const booksWrapper = document.querySelector('.books');
 
-const books = getBooks();
+//document.body.classList += ' books__loading'
+const books = await getBooks();
+//document.body.classList.remove('books__loading')
 
 if (filter === "LOW_TO_HIGH") {
-books.sort((a, b) => a.originalPrice - b.originalPrice);
+books.sort((a, b) => (a.salePrice || a.originalPrice) - (b.salePrice || b.originalPrice));
 }
 else if (filter === "HIGH_TO_LOW") {
-books.sort((a, b) => b.originalPrice - a.originalPrice);
+books.sort((a, b) => (b.salePrice || b.originalPrice) - (a.salePrice || a.originalPrice));
 }
 else if (filter === "RATING") {
 books.sort((a, b) => b.rating - a.rating);
@@ -77,103 +79,107 @@ setTimeout(() => {
 });
 // FAKE DATA
 function getBooks() {
-  return [
-    {
-      id: 1,
-      title: "Crack the Coding Interview",
-      url: "assets/crack the coding interview.png",
-      originalPrice: 49.95,
-      salePrice: 14.95,
-      rating: 4,
-    },
-    {
-      id: 2,
-      title: "Atomic Habits",
-      url: "assets/atomic habits.jpg",
-      originalPrice: 39.95,
-      salePrice: null,
-      rating: 5,
-    },
-    {
-      id: 3,
-      title: "Deep Work",
-      url: "assets/deep work.jpeg",
-      originalPrice: 29.95,
-      salePrice: 14.95,
-      rating: 5,
-    },
-    {
-      id: 4,
-      title: "The 10X Rule",
-      url: "assets/book-1.jpeg",
-      originalPrice: 44.95,
-      salePrice: 19.95,
-      rating: 4,
-    },
-    {
-      id: 5,
-      title: "Be Obsessed Or Be Average",
-      url: "assets/book-2.jpeg",
-      originalPrice: 89.95,
-      salePrice: 9.95,
-      rating: 1,
-    },
-    {
-      id: 6,
-      title: "Rich Dad Poor Dad",
-      url: "assets/book-3.jpeg",
-      originalPrice: 70.95,
-      salePrice: 14.95,
-      rating: 4.5,
-    },
-    {
-      id: 7,
-      title: "Cashflow Quadrant",
-      url: "assets/book-4.jpeg",
-      originalPrice: 11.95,
-      salePrice: 9.95,
-      rating: 4,
-    },
-    {
-      id: 8,
-      title: "48 Laws of Power",
-      url: "assets/book-5.jpeg",
-      originalPrice: 48.48,
-      salePrice: 19.95,
-      rating: 5,
-    },
-    {
-      id: 9,
-      title: "The 5 Second Rule",
-      url: "assets/book-6.jpeg",
-      originalPrice: 34.95,
-      salePrice: null,
-      rating: 4,
-    },
-    {
-      id: 10,
-      title: "Your Next Five Moves",
-      url: "assets/book-7.jpg",
-      originalPrice: 39.95,
-      salePrice: null,
-      rating: 4,
-    },
-    {
-      id: 11,
-      title: "Mastery",
-      url: "assets/book-8.jpeg",
-      originalPrice: 29.95,
-      salePrice: null,
-      rating: 4.5,
-    },
-    {
-      id: 12,
-      title: "Can't Hurt Me",
-      url: "assets/david goggins.jpeg",
-      originalPrice: 29.95,
-      salePrice: null,
-      rating: 5,
-    }
-  ];
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      resolve([
+        {
+          id: 1,
+          title: "Crack the Coding Interview",
+          url: "assets/crack the coding interview.png",
+          originalPrice: 49.95,
+          salePrice: 14.95,
+          rating: 4,
+        },
+        {
+          id: 2,
+          title: "Atomic Habits",
+          url: "assets/atomic habits.jpg",
+          originalPrice: 39.95,
+          salePrice: null,
+          rating: 5,
+        },
+        {
+          id: 3,
+          title: "Deep Work",
+          url: "assets/deep work.jpeg",
+          originalPrice: 29.95,
+          salePrice: 14.95,
+          rating: 5,
+        },
+        {
+          id: 4,
+          title: "The 10X Rule",
+          url: "assets/book-1.jpeg",
+          originalPrice: 44.95,
+          salePrice: 19.95,
+          rating: 4,
+        },
+        {
+          id: 5,
+          title: "Be Obsessed Or Be Average",
+          url: "assets/book-2.jpeg",
+          originalPrice: 89.95,
+          salePrice: 9.95,
+          rating: 1,
+        },
+        {
+          id: 6,
+          title: "Rich Dad Poor Dad",
+          url: "assets/book-3.jpeg",
+          originalPrice: 70.95,
+          salePrice: 14.95,
+          rating: 4.5,
+        },
+        {
+          id: 7,
+          title: "Cashflow Quadrant",
+          url: "assets/book-4.jpeg",
+          originalPrice: 11.95,
+          salePrice: 9.95,
+          rating: 4,
+        },
+        {
+          id: 8,
+          title: "48 Laws of Power",
+          url: "assets/book-5.jpeg",
+          originalPrice: 48.48,
+          salePrice: 19.95,
+          rating: 5,
+        },
+        {
+          id: 9,
+          title: "The 5 Second Rule",
+          url: "assets/book-6.jpeg",
+          originalPrice: 34.95,
+          salePrice: null,
+          rating: 4,
+        },
+        {
+          id: 10,
+          title: "Your Next Five Moves",
+          url: "assets/book-7.jpg",
+          originalPrice: 39.95,
+          salePrice: null,
+          rating: 4,
+        },
+        {
+          id: 11,
+          title: "Mastery",
+          url: "assets/book-8.jpeg",
+          originalPrice: 29.95,
+          salePrice: null,
+          rating: 4.5,
+        },
+        {
+          id: 12,
+          title: "Can't Hurt Me",
+          url: "assets/david goggins.jpeg",
+          originalPrice: 29.95,
+          salePrice: null,
+          rating: 5,
+        },
+      ]);
+    }, 1500); 
+  });
 }
 
